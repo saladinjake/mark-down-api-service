ebfce21807e4a593055a4fabbeb2d622e47ef0e2
saladinjake <juwavicto@gmail.com>
2021-03-12 10:00:00 +0100

    feat: implement skeleton UI for markdown editor and preview

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <h1>MarkDownIt</h1>
+      <p>Live Markdown Previewer</p>
+    </div>
+    
+    <div class="main-content">
+      <div class="editor-pane">
+        <h3>Editor</h3>
+        <textarea 
+          v-model="markdownText" 
+          @input="handleInput"
+          placeholder="Type your markdown here..."
+        ></textarea>
+      </div>
+      
+      <div class="preview-pane">
+        <h3>Preview</h3>
+        <div v-html="renderedHtml"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      markdownText: '# Welcome to MarkDownIt\n\nStart typing to see the preview!',
+      renderedHtml: ''
+    };
+  },
+  methods: {
+    async handleInput() {
+      try {
+        const response = await axios.post('/api/render', {
+          markdown: this.markdownText
+        });
+        this.renderedHtml = response.data.data;
+      } catch (err) {
+        console.error('Render error:', err);
+      }
+    }
+  },
+  mounted() {
+    this.handleInput();
   }
 }
 </script>
 
 <style>
+/* Skeleton UI - No premium styles yet */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
+}
+.main-content {
+  display: flex;
+  gap: 20px;
+}
+.editor-pane, .preview-pane {
+  flex: 1;
+}
+textarea {
+  width: 100%;
+  height: 400px;
 }
 </style>
