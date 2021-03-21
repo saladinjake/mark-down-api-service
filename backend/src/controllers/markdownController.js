7fc843f2fd230f9716bd2e626b32402ff7a9e41d
saladinjake <juwavicto@gmail.com>
2021-03-21 16:00:00 +0100

    feat: add redis caching and complete docker orchestration. Project 4 complete.

--- backend/src/controllers/markdownController.js
+++ backend/src/controllers/markdownController.js
@@ -1,9 +1,18 @@
+const redis = require('redis');
+const { promisify } = require('util');
 const { renderMarkdown } = require('../services/markdownService');
 
-// @desc    Render markdown to HTML
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+const client = redis.createClient(REDIS_URL);
+
+client.on('error', (err) => console.error('Redis Client Error', err));
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.setex).bind(client);
+
+// @desc    Render markdown to HTML (with Redis caching)
 // @route   POST /api/render
-// @access  Public
-exports.render = (req, res) => {
+exports.render = async (req, res) => {
   try {
     const { markdown } = req.body;
     
@@ -11,9 +20,21 @@ exports.render = (req, res) => {
       return res.status(400).json({ success: false, error: 'Markdown content is required' });
     }
     
+    // Create a cache key (hash would be better but simple string for now)
+    const cacheKey = `md:${Buffer.from(markdown).toString('base64').substring(0, 50)}`;
+    
+    // Check cache
+    const cachedHtml = await getAsync(cacheKey);
+    if (cachedHtml) {
+      return res.status(200).json({ success: true, data: cachedHtml, cached: true });
+    }
+    
     const html = renderMarkdown(markdown);
     
-    res.status(200).json({ success: true, data: html });
+    // Cache for 1 hour
+    await setAsync(cacheKey, 3600, html);
+    
+    res.status(200).json({ success: true, data: html, cached: false });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
   }
